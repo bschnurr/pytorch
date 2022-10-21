b73dbfea6f3b1e56084e9f20b9f6fabc938c19e5
Catherine Lee <csl@fb.com>
2022-10-21 15:13:26 -0700

    update

--- .circleci/scripts/python_doc_push_script.sh
+++ .circleci/scripts/python_doc_push_script.sh
@@ -136,6 +136,7 @@ git status
 
 # push to a temp branch first to trigger CLA check and satisfy branch protections
 git push -u origin HEAD:csl/temp-branch-py -f
+curl -X PATCH -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/pytorch/pytorch.github.io/pull/1155 -d '{"state":"open"}'
 gh pr reopen csl/temp-branch-py
 sleep 30
 git push -u origin HEAD:csl/temp-branch-py-1
